5c043ea972edb17e936ff18cbeabf4e87572709c
PaoloTarcisio <paoloc50@gmail.com>
2023-11-30 15:10:01 +0100

    Completamento esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nome = prompt("inserisci il tuo nome");
+const cognome = prompt("inserisci il tuo cognome");
+const colore = prompt("inserisci il tuo colore preferito");
+
+
+// document.getElementById('pwd').innerHTML="La tua Password sarà: " + nome + cognome + colore + "114";
+
+document.writeln("La tua Password sarà: " + nome + cognome + colore + "114")
+
+alert("La tua Password sarà: " + nome + cognome + colore + "114")
